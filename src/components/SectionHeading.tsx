4ce97fee430e4675bb9e9a2c55b34fab62087fff
luisRiofrio <lriofrio915@gmail.com>
2023-10-10 06:28:25 -0400

    project section

---
+++ src/components/SectionHeading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type SectionHeadingProps = {
+  children: React.ReactNode;
+};
+
+const SectionHeading = ({children} : SectionHeadingProps) => {
+  return (
+    <div className="text-3xl font-medium capitalize mb-8 text-center">
+      {children}
+    </div>
+  );
+};
+
+export default SectionHeading;
