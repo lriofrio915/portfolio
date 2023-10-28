3b3fd6a3fae69e7e9b169019049791d8fdfc1513
luisRiofrio <lriofrio915@gmail.com>
2023-10-28 07:58:25 -0400

    luisRiofrio change About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,8 +19,9 @@ const About = () => {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">Chemical Engineering</span>, I decided to
+        study a master's degree in software development to pursue my passion for
+        programming. I enrolled for two coding bootcamps and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
@@ -29,18 +30,18 @@ const About = () => {
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        . I am also familiar with TypeScript and Tailwind CSS. I am always
+        looking to learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I'm not coding</span>, I really enjoy
+        investing my time backtesting investment strategies, I love the
+        financial markets. I also enjoy{" "}
+        <span className="font-medium">reading</span>. I am currently
+        reading about{" "}
+        <span className="font-medium">the power of good habits</span>. Atomic habits.
       </p>
     </motion.section>
   );
