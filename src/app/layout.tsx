b1a7fec2b570efdc5305bce4b578a194895f85f9
luisRiofrio <lriofrio915@gmail.com>
2023-10-28 22:04:18 -0400

    update themeSwitch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import ActiveSectionContextProvider from "../context/active-section-context";
 import ThemeContextProvider from "../context/theme-context";
 import Footer from "../components/Footer";
 import { Toaster } from "react-hot-toast";
+import ThemeSwitch from "../components/ThemeSwitch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
             {children}
             <Footer />
             <Toaster position="top-right" />
+            <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
