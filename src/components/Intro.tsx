f8ac57d56b9f3c5586bc6dfb4418e2cddd7e6c88
luisRiofrio <lriofrio915@gmail.com>
2023-11-03 18:44:13 -0400

    no ticked again

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import useSectionInView from "../lib/hooks";
 import { useActiveSectionContext } from "../context/active-section-context";
-import IconPic from "../public/luisRiofrioIcon.jpg";
+import IconPic from "@/public/luisRiofrioIcon.jpg";
 
 const Intro = () => {
   const { ref } = useSectionInView("Home", 0.5);
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,10 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "../public/capital8.webp";
-import rmtdevImg from "../public/consultorioMedrano.webp";
-import wordanalyticsImg from "../public/ecommerceCanna.webp";
-import ecommerceAdminImg from "../public/ecommerceAdmin.webp";
+import corpcommentImg from "@/public/capital8.webp";
+import rmtdevImg from "@/public/consultorioMedrano.webp";
+import wordanalyticsImg from "@/public/ecommerceCanna.webp";
+import ecommerceAdminImg from "@/public/ecommerceAdmin.webp";
 
 export const links = [
   {
